08642df8818fbffdb53ce2b0102562719ad5ac0c
dpmerino <diegopaul7@outlook.com>
2021-07-27 10:47:01 -0500

    print service

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -21,11 +21,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  paciente: Paciente = {}
+  vacunas: Vacuna[] = [];
+  profesionales: any[] = [];
+  centros: Centro[] = [];
+  pacientes: Paciente[] = [];
+  registros: Registro[] = [];
+
+  registro: Registro = {};
+  vacuna: Vacuna = {};
   profesional: Profesional = {}
-  centro: Centro = {}
-  registro: Registro = {}
-  vacuna: Vacuna = {}
+  paciente: Paciente = {};
+  centro: Centro = {};
+  aux: any[] = [];
   bandera: boolean = false;
 
   idPaciente = '';
@@ -40,6 +47,7 @@ export class RegistroComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
+    this.llenarCollections();
   }
 
   openDialogError() {
@@ -51,6 +59,7 @@ export class RegistroComponent implements OnInit {
 
   findPaciente(ob: Event){
     console.log(this.idPaciente)
+    this.fetchPaciente();
     //ejecutar
   }
   openInfo(){
@@ -62,34 +71,93 @@ export class RegistroComponent implements OnInit {
   }
 
   fetchPaciente(){
-    this.paciente.c_i = this.idPaciente
-    this.pacienteService.getPacienteById(this.paciente).subscribe(
+    /*this.paciente.c_i = this.idPaciente
+    /*this.pacienteService.getPacienteById(this.paciente).subscribe(
       result => {
         let aux: any[] = result;
         if(aux[0] != undefined){
           this.paciente = aux[0]
+          this.bandera = true;
           //ejecutar fetch(s)
           //this.openDialog()
-
         }else{
-          this.openDialogError()
+          //this.openDialogError()
+        }
+      }
+    )*/
+    this.paciente.c_i = this.idPaciente;
+    this.pacienteService.getAllPaciente().subscribe(
+      result => {
+        this.pacientes = result;
+        for (let p of this.pacientes){
+          if(this.paciente.c_i === p.c_i){
+              this.paciente.id = p.id;
+              this.paciente = p;
+              this.bandera = true;
+          }
         }
       }
     )
   }
   fetchRegistro(){
     //Aqui llenar objeto registro usando datos del paciente en obj paciente
+    for(let r of this.registros){
+      if(r.id_paciente === this.paciente.id){
+        this.registro = r;
+      }
+    }
   }
   fetchVacuna(){
     //Aqui llenar obj Vacuna
+    for (let v of this.vacunas){
+      if(v.id === this.registro.id_vacuna){
+        this.vacuna = v;
+      }
+    }
   }
   fetchRecinto(){
     //Aqui llenar obj Recinto
+    for (let c of this.centros){
+      if(c.id === this.registro.id_centro){
+        this.centro = c;
+      }
+    }
   }
   fetchProfesional(){
     //Aqui llenar obj profesional usar localstorage
+    for(let p of this.profesionales){
+      if(p.id === this.registro.id_profesional){
+        this.profesional = p;
+      }
+    }
   }
-  
+  llenarCollections(){
+    this.profesionalService.getAllProfesional().subscribe(
+      result => {
+        this.profesionales = result;
+      }
+    )
+    this.registroService.getRegistros().subscribe(
+      result => {
+        this.registros = result;
+      }
+    )
+    this.pacienteService.getAllPaciente().subscribe(
+      result => {
+        this.pacientes = result;
+      }
+    )
+    this.centroService.getAllCentro().subscribe(
+      result => {
+        this.centros = result;
+      }
+    )
+    this.vacunaService.getAllVacuna().subscribe(
+      result => {
+        this.vacunas = result;
+      }
+    )
+  }
 
   openDialog(){
     //llamar funcion para llenar vacuna y recinto
@@ -98,8 +166,8 @@ export class RegistroComponent implements OnInit {
     const dialogRef = this.dialog.open(ConfirmRegistroComponent, {
       
       data: { 
-        btn_text: 'Imprimir'
-        /*
+        btn_text: 'Imprimir',
+        
         idPaciente: this.paciente.c_i,
         nombresPaciente: this.paciente.nombres,
         apellidosPaciente: this.paciente.apellidos,
@@ -116,7 +184,7 @@ export class RegistroComponent implements OnInit {
         provincia: this.centro.provincia,
         ciudad: this.centro.ciudad,
         recinto: this.centro.recinto
-        */
+        
       }
       
     });
