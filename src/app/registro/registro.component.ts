6941d7cacd7fba86bf382b01f94935e48a3293a5
paxasaval <paxasaval1003@gmail.com>
2021-07-27 09:31:47 -0500

    fetch_function_registro

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,3 +1,14 @@
+import { NoUserComponent } from './../no-user/no-user.component';
+import { Vacuna } from './../models/vacuna';
+import { Registro } from './../models/registro';
+import { Centro } from './../models/centro';
+import { Profesional } from './../models/profesional';
+import { RegistroService } from './../service/registro.service';
+import { PacienteService } from './../service/paciente.service';
+import { ProfesionalesService } from './../service/profesionales.service';
+import { CentrosService } from './../service/centros.service';
+import { VacunaService } from './../service/vacuna.service';
+import { Paciente } from './../models/paciente';
 import { ConfirmRegistroComponent } from './../confirm-registro/confirm-registro.component';
 import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
@@ -9,10 +20,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  paciente: Paciente = {}
+  profesional: Profesional = {}
+  centro: Centro = {}
+  registro: Registro = {}
+  vacuna: Vacuna = {}
+
+  idPaciente = '';
+
+
+  constructor(private dialog: MatDialog,
+    private vacunaService: VacunaService,
+    private centroService: CentrosService,
+    private profesionalService: ProfesionalesService,
+    private pacienteService: PacienteService,
+    private registroService: RegistroService,
+    ) { }
 
   ngOnInit(): void {
   }
+
+  openDialogError() {
+    const dialogRef = this.dialog.open(NoUserComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  fetchPaciente(){
+    this.paciente.c_i = this.idPaciente
+    this.pacienteService.getPacienteById(this.paciente).subscribe(
+      result => {
+        let aux: any[] = result;
+        if(aux[0] != undefined){
+          this.paciente = aux[0]
+          //ejecutar fetch(s)
+          this.openDialog()
+
+        }else{
+          this.openDialogError()
+        }
+      }
+    )
+  }
+  fetchRegistro(){
+    //Aqui llenar objeto registro usando datos del paciente en obj paciente
+  }
+  fetchVacuna(){
+    //Aqui llenar obj Vacuna
+  }
+  fetchRecinto(){
+    //Aqui llenar obj Recinto
+  }
+  fetchProfesional(){
+    //Aqui llenar obj profesional usar localstorage
+  }
+  
+
   openDialog(){
     //llamar funcion para llenar vacuna y recinto
    
